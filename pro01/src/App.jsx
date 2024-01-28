a70b491ac0e92bf80a67dd590f9eba80d92dd036
MP Web Developer <mprwdev@gmail.com>
2024-01-28 14:28:49 +0200

    event hanling

--- pro01/src/App.jsx
+++ pro01/src/App.jsx
@@ -7,8 +7,11 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  function handleSelect() {
-    console.log('btn clicked - app');
+  let tabContent = 'Please click a button';
+
+  function handleSelect(selectedButton) {
+    tabContent = selectedButton;
+    console.log(tabContent);
   }
 
   return (
@@ -36,11 +39,14 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
