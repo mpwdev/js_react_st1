2afa249927b91dd95233757d4d5c5cb5c8ed927f
MP Web Developer <mprwdev@gmail.com>
2024-01-29 12:41:18 +0200

    multiple jsx slots

--- pro02/src/App.jsx
+++ pro02/src/App.jsx
@@ -1,16 +1,8 @@
-// import { useState } from 'react';
-
-// import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
-// import CoreConcept from './components/CoreConcept.jsx';
 import CoreConcepts from './components/CoreConcepts.jsx';
-// import TabButton from './components/TabButton.jsx';
-// import { EXAMPLES } from './data.js';
 import Examples from './components/Examples.jsx';
 
 function App() {
-
-
   return (
     <>
       <Header />
