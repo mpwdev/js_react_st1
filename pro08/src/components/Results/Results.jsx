b5a2a06c1850f4a7c19ac66ac4da753354614b89
MP Web Developer <mprwdev@gmail.com>
2024-01-31 15:20:12 +0200

    table header html fixed

--- pro08/src/components/Results/Results.jsx
+++ pro08/src/components/Results/Results.jsx
@@ -15,11 +15,13 @@ export default function Results({ input }) {
   return (
     <table id="result">
       <thead>
-        <th>Year</th>
-        <th>Investment Value</th>
-        <th>Interest (Year)</th>
-        <th>Total Interest</th>
-        <th>Invested Capital</th>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
       </thead>
       <tbody>
         {resultsData.map((yearData) => {
