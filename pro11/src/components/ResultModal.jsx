75057fcb220688bd757df7908d1c0a8be63e8537
MP Web Developer <mprwdev@gmail.com>
2024-02-05 20:48:18 +0200

    close modal with esc

--- pro11/src/components/ResultModal.jsx
+++ pro11/src/components/ResultModal.jsx
@@ -7,8 +7,9 @@ const ResultModal = forwardRef(function ResultModal(
   // const dialog = useRef();
   const dialogModal = useRef();
 
-	const userLost = remainingTime <= 0;
-	const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -19,13 +20,15 @@ const ResultModal = forwardRef(function ResultModal(
   });
 
   return (
-    <dialog ref={dialogModal} className="result-modal">
+    <dialog ref={dialogModal} className="result-modal" onClose={onReset}>
       {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
+        You stopped the timer with{' '}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
