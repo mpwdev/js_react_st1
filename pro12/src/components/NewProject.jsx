7dd4738b8c5270d7bf375611635d2d4cdf1ec2d6
MP Web Developer <mprwdev@gmail.com>
2024-02-07 13:21:45 +0200

    modal styling

--- pro12/src/components/NewProject.jsx
+++ pro12/src/components/NewProject.jsx
@@ -34,8 +34,10 @@ export default function NewProject({ onAdd }) {
   return (
     <>
       <Modal ref={modal} buttonCaption="Okay">
-        <h2>Invalid Input</h2>
-        <p>Please make sure you provide a valid value for every field.</p>
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-700 mb-8">
+          Please make sure you provide a valid value for every field.
+        </p>
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
