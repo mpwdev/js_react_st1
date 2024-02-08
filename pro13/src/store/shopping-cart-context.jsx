6ae6e44dac4b9aeb4ec367fc986fea6ae2bdeb3d
MP Web Developer <mprwdev@gmail.com>
2024-02-08 13:43:18 +0200

    use context everywere

--- pro13/src/store/shopping-cart-context.jsx
+++ pro13/src/store/shopping-cart-context.jsx
@@ -2,5 +2,6 @@ import { createContext } from 'react';
 
 export const CartContext = createContext({
   items: [],
-  addItemToCart: () => {}, // for dropdown advice in IDE
+  addItemToCart: () => {}, // for autocomplition in IDE
+  updateItemQuantity: () => {}, // for autocomplition in IDE
 });
