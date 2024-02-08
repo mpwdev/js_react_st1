9ebe243977595f84e57b03ef33e193c152cc124a
MP Web Developer <mprwdev@gmail.com>
2024-02-08 12:44:55 +0200

    access context data

--- pro13/src/App.jsx
+++ pro13/src/App.jsx
@@ -5,6 +5,8 @@ import Shop from './components/Shop.jsx';
 import Product from './components/Product.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
 
+import { CartContext } from './store/shopping-cart-context.jsx';
+
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
     items: [],
@@ -67,7 +69,7 @@ function App() {
   }
 
   return (
-    <>
+    <CartContext.Provider value={{ items: [] }}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -79,7 +81,7 @@ function App() {
           </li>
         ))}
       </Shop>
-    </>
+    </CartContext.Provider>
   );
 }
 
