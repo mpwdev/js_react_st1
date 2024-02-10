f8618371d5197d73f584334d80e7288d05d624fb
MP Web Developer <mprwdev@gmail.com>
2024-02-10 15:10:11 +0200

    show the answers and select active

--- pro15/src/components/Quiz.jsx
+++ pro15/src/components/Quiz.jsx
@@ -1,12 +1,36 @@
 import { useState } from 'react';
 
+import QUESTIONS from '../questions.js';
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  // const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    console.log(selectedAnswer);
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
   return (
-    <div>
-      <p>Currently active question</p>
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+            return (
+              <li key={answer} className="answer">
+                <button onClick={() => handleSelectAnswer(answer)}>
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
