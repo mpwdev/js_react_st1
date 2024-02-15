d8f5a094d57eb54fdcffdbc7f0f07c5e2ef597ce
MP Web Developer <mprwdev@gmail.com>
2024-02-15 22:21:33 +0200

    get places with fetch and async await

--- pro18/src/components/AvailablePlaces.jsx
+++ pro18/src/components/AvailablePlaces.jsx
@@ -5,17 +5,25 @@ import Places from './Places.jsx';
 export default function AvailablePlaces({ onSelectPlace }) {
   const [avalablePlaces, setAvailablePlaces] = useState([]);
 
-	useEffect(() => {
-		fetch('http://localhost:3000/places').then((response) => {
-			return response.json();
-		})
-		.then((resData) => {
-			setAvailablePlaces(resData.places);
-		});
+  useEffect(() => {
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
 
-	}, []);
+		fetchPlaces();
+  }, []);
 
-  
+  // useEffect(() => {
+  // 	fetch('http://localhost:3000/places').then((response) => {
+  // 		return response.json();
+  // 	})
+  // 	.then((resData) => {
+  // 		setAvailablePlaces(resData.places);
+  // 	});
+
+  // }, []);
 
   return (
     <Places
