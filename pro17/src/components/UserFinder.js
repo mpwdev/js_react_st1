40fdf316dee0bf81709e69ba270eb0df07d6a906
MP Web Developer <mprwdev@gmail.com>
2024-02-15 18:35:47 +0200

    example of using context in class based component

--- pro17/src/components/UserFinder.js
+++ pro17/src/components/UserFinder.js
@@ -1,15 +1,18 @@
 import { Fragment, useState, useEffect, Component } from 'react';
 
-import Users from './Users';
+import Users from './Users.js';
 import classes from './UserFinder.module.css';
+import UsersContext from '../store/users-context.js';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
+// const DUMMY_USERS = [
+//   { id: 'u1', name: 'Max' },
+//   { id: 'u2', name: 'Manuel' },
+//   { id: 'u3', name: 'Julie' },
+// ];
 
 class UserFinder extends Component {
+	static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
@@ -20,13 +23,13 @@ class UserFinder extends Component {
 
   componentDidMount() { // only run once  - when first render
     // send http request...
-    this.setState({ filteredUsers: DUMMY_USERS });
+    this.setState({ filteredUsers: this.context.users });
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
