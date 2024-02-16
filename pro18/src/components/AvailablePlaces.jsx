cc786cd8628c666921a1dc80d2ee42d12e05286c
MP Web Developer <mprwdev@gmail.com>
2024-02-16 11:58:39 +0200

    outsorce fetch code into separate file

--- pro18/src/components/AvailablePlaces.jsx
+++ pro18/src/components/AvailablePlaces.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Error from './Error.jsx';
 import Places from './Places.jsx';
 import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -14,25 +15,30 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch('http://localhost:3000/places');
-        const resData = await response.json();
+        // const response = await fetch('http://localhost:3000/places');
+        // const resData = await response.json();
 
-        if (!response.ok) {
-          throw new Error('Failed to fetch places');
-        }
+        // if (!response.ok) {
+        //   throw new Error('Failed to fetch places');
+        // }
+       const places = await fetchAvailablePlaces();
 
-				navigator.geolocation.getCurrentPosition((position) => {
-					const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude);
-					setAvailablePlaces(sortedPlaces);
-					setIsFetching(false);
-				});
-       
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            // resData.places,
+						places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({
           message:
             error.message || 'Could not fetch places, please try again later.',
         });
-				setIsFetching(false);
+        setIsFetching(false);
       }
 
       // setIsFetching(false);
