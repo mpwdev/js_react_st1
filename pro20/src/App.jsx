dfd00f4035fe7b5b96617afc2d7830cf0829321e
MP Web Developer <mprwdev@gmail.com>
2024-03-01 16:37:16 +0200

    validation on submit

--- pro20/src/App.jsx
+++ pro20/src/App.jsx
@@ -1,7 +1,8 @@
 import Header from './components/Header.jsx';
 // import Login from './components/Login.jsx';
-import Login from './components/StateLogin.jsx';
+// import Login from './components/StateLogin.jsx';
 // import Signup from './components/Signup.jsx';
+import Login from './components/RefLogin.jsx';
 
 function App() {
   return (
