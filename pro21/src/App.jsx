5d4087d921cfbb0d904c6dd711eac2132b5edc91
MP Web Developer <mprwdev@gmail.com>
2024-03-12 11:38:38 +0200

    open close modal functionality

--- pro21/src/App.jsx
+++ pro21/src/App.jsx
@@ -1,13 +1,18 @@
 import Header from './components/Header.jsx';
 import Meals from './components/Meals.jsx';
 import { CartContextProvider } from './store/CartContext.jsx';
+import { UserProgressContextProvider } from './store/UserProgressContext.jsx';
+import Cart from './components/Cart.jsx';
 
 function App() {
   return (
-    <CartContextProvider>
-      <Header></Header>
-      <Meals></Meals>
-    </CartContextProvider>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header></Header>
+        <Meals></Meals>
+        <Cart></Cart>
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
