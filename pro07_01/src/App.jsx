fe4d1f657433da71e5d3d0a122ac0405c6219bcc
MP Web Developer <mprwdev@gmail.com>
2024-03-22 20:55:23 +0200

    player component

--- pro07_01/src/App.jsx
+++ pro07_01/src/App.jsx
@@ -1,22 +1,12 @@
+import Player from './components/Player.jsx';
+
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player">
-              <span className="player-name">Player 1</span>
-              <span className="player-symbol">X</span>
-            </span>
-            <button>Edit</button>
-          </li>
-          <li>
-            <span className="player">
-              <span className="player-name">Player </span>
-              <span className="player-symbol">O</span>
-            </span>
-            <button>Edit</button>
-          </li>
+          <Player name="Player 1" symbol="X"></Player>
+          <Player name="Player 2" symbol="O"></Player>
         </ol>
       </div>
     </main>
