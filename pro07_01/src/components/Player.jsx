5252a70acdd8e3456f4a287ae2cb10527287bcbe
MP Web Developer <mprwdev@gmail.com>
2024-03-22 22:08:12 +0200

    player add button click handler

--- pro07_01/src/components/Player.jsx
+++ pro07_01/src/components/Player.jsx
@@ -1,11 +1,25 @@
-export default function Player({name, symbol}) {
+import { useState } from 'react';
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+	let PlayerName = <span className="player-name">{name}</span>;
+	
+	if (isEditing) {
+		PlayerName = <input type="text" required></input>;
+	}
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {PlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
